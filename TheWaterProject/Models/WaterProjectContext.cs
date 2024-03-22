3263b98521a2ed2a731148cb1150e525cb7c3c8c
jfinch8 <jenessa.finch08@gmail.com>
2024-03-22 15:08:59 -0600

    Add project files.

---
+++ TheWaterProject/Models/WaterProjectContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TheWaterProject.Models
+{
+    public class WaterProjectContext : DbContext
+    {
+        public WaterProjectContext (DbContextOptions<WaterProjectContext> options) : base(options) { }
+
+        public DbSet<Project> Projects { get; set; }
+
+
+    }
+}
